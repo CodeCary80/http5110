0e9ee31d59c2e50faf729afb78995cf136be265a
Tianrui Zhu <weltandpuzzle@gmail.com>
2025-04-15 18:10:03 -0400

    week12&13

---
+++ laralms/resources/views/students/index.blade.php
@@ -0,0 +1,35 @@
+@extends('layout')
+@section('content')
+
+<h1>All Students</h1>
+    <ul>
+        @foreach ( $students as $student)
+        <li>{{ $student -> fname}} {{ $student -> lname}} |
+            <a href="{{ route('students.edit', $student -> id )}}">Edit</a>
+            <form action="{{ route('students.destroy', $student -> id) }}" method="POST">
+                {{ csrf_field() }}
+                @method('DELETE')
+                <input type="submit" value="Delete">
+           </form>
+        </li>
+        @endforeach
+    </ul>
+@endsection
+
+
+<!--
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    All Students
+     @foreach ( $students as $student)
+        {{ $student -> fname}} <br>
+    @endforeach
+</body>
+</html>
+-->
---
+++ laralms/routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+
+use App\Http\Controllers\CourseController;
+use App\Http\Controllers\StudentController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//Route::get('/students', function () {
+//    return view('students.index');
+// });
+
+Route::resource('students', StudentController::class);
+
+Route::resource('courses', CourseController::class);
